9ce8780d6756e80f3b039bad85ab58258515f42f
tomiarg <tmoyano@claro.com.ar>
2022-09-28 11:28:41 -0300

    first commit

---
+++ src/components/componenteFuncion/componenteFuncion.jsx
@@ -0,0 +1,12 @@
+import "./componenteFuncion.css"
+
+export const ComponenteFuncion = () =>{
+    return (
+        <div className ="nav">
+           <div >
+            primer componente tipo Funcion
+           </div>
+           <p>parrafo</p>
+        </div>
+    )   
+}
